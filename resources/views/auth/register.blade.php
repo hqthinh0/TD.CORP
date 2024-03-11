8f638c729f3a6b6c495f7d1aff1807d1ab61b598
Caily.QuocThinh <quocthinh_web@caily.com.vn>
2024-03-11 09:25:30 +0700

    Update UserName

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,10 +9,18 @@
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
+        
+        <!-- userName -->
+        <div class="mt-4">
+            <x-input-label for="name" :value="__('User name')" />
+            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autocomplete="username" />
+          <!-- <x-input-error :messages="$errors->get('email')" class="mt-2" /> -->
+        </div>
+
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
