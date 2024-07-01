43439ee95f65eb42b6c44bbdcc5b7f65854c064a
Caily.QuocThinh <quocthinh_web@caily.com.vn>
2024-07-01 10:12:10 +0700

    no message

---
+++ resources/views/frontend/document/404.blade.php
@@ -0,0 +1,16 @@
+@extends('frontend.main_master')
+@section('pageTitle', '404 Error')
+@section('main')
+
+
+<div id="l-main">
+	<section class="error" >
+   <h2 class="hdg-lv2 hdg-lv2--center" > <span class="color-primary">404</span> <img src="{{ asset('frontend/assets/images/line_h2.png')}}" /></h2>
+    <p class="txt-lead hdg-lv2--center">Xin lỗi! trang đang được cập nhật vui lòng quay lại sau</p>
+	</section>    
+
+</div>
+<!-- /#main -->
+<!-- /#container -->
+
+@endsection
